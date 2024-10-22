a6cf3c7452563a3797088eb5fceab6bf9d04771f
sreInk <srinknok@gmail.com>
2024-10-22 09:18:32 +0530

    w

---
+++ Nato Pincode.py
@@ -0,0 +1,8 @@
+bro_code = {"India":2000, 'Nato': 2122,'Japan':200012}
+print("The country Code is -")
+print(bro_code.get("India","Not Found"))
+print("The country Code is of Nato -")
+print(bro_code.get("Nato","Not Found"))
+print("The country Code of Japan is -")
+print(bro_code.get("Austrulla","Not Found"))
+
